93593095045cc6086932ed90969ed7f88a479a85
NAbdullokh <country.abu@gmail.com>
2022-09-15 16:56:29 +0500

    pagenation

--- src/components/AllFlowers/FlowerList/style.js
+++ src/components/AllFlowers/FlowerList/style.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: space-evenly;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(308px, 1fr));
+  grid-gap: 1rem;
+  align-items: start;
 `;
--- src/components/AllFlowers/index.jsx
+++ src/components/AllFlowers/index.jsx
@@ -10,27 +10,22 @@ import FLowerList from "./FlowerList";
 const AllFlowers = () => {
   const { data, loading } = useContext(BackData);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(8);
+  const [postsPerPage, setPostsPerPage] = useState(9);
   const lastPostIndex = currentPage * postsPerPage;
   const firstPostIndex = lastPostIndex - postsPerPage;
   const currentPosts = data.slice(firstPostIndex, lastPostIndex);
 
   return (
     <Container>
-      <Body>
-        {loading ? <Loader /> : <FLowerList coinsData={currentPosts} />}
-        <Pagination
-          totalPosts={data.length}
-          postsPerPage={postsPerPage}
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage}
-        />
-      </Body>
+      {loading ? <Loader /> : <FLowerList coinsData={currentPosts} />}
+      <Pagination
+        totalPosts={data.length}
+        postsPerPage={postsPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
     </Container>
   );
 };
 
 export default AllFlowers;
-
-// data.map((value) => {
-//   return <Card key={value.id} data={value} />;
